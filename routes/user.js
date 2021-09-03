959298522ad91a601a7a66ac406f83ac5a5d92fd
ajaypradeep <ajaypradeep1712@gmail.com>
2021-09-03 12:42:59 +0530

    server ready

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+router.get('/',(req,res)=>{
+
+    res.send('HI WELCOME TO SCH SERVER')
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,40 @@
-
-
-const express =require('express');
- const bodyParser =require('body-parser');
-const cors= require('cors')
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { readdirSync } from "fs";
+const morgan = require("morgan");
+import cookieParser from "cookie-parser";
 const path = require("path");
 const app = express();
-const morgan=require('morgan')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// connect DB
+// mongoose
+//   .connect(process.env.MONGO_DB, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//   })
+//   .then(() => console.log("MongoDB is connected"))
+//   .catch((err) => console.log("DB CONNECTION ERROR", err));
+
+
+
+
+
 
 
 
@@ -13,25 +42,18 @@ const morgan=require('morgan')
 app.use(cors());
 //serve static files
 app.use(express.static("images"));
+
 app.use("/images", express.static(path.join(__dirname, "/images")));
 app.use(express.json({ limit: "50mb" }));
 app.use(morgan("dev"));
 
 
+app.use(cookieParser());
 
+// route - This func is for importing routes files automaticaly. so we dont need to import separately
+readdirSync("./routes").map((r) => app.use("/", require(`./routes/${r}`)));
 
-app.get("/", (req, res, next) => {
-  
- res.send("Welcome to project SCH");
+// port
+const port = process.env.PORT || 2000;
 
-});
-
-
-
-
-app.listen(7000, () => {
-
-  console.log(`Server is running`);
-
-
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
