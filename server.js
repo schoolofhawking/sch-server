ded5f39c17f11c8494a9e221fc80969f862100a0
schoolofhawking <94289884+schoolofhawking@users.noreply.github.com>
2022-01-12 16:31:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
   .catch((err) => console.log("DB CONNECTION ERROR", err));
 
 // apply middlewares
-app.use(cors({credentials: true, origin: "*"}));
+app.use(cors({credentials: false, origin: "*"}));
 
 
 //serve static files
