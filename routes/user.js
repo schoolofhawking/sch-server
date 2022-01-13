0063ff3eeb071e37b30cc6976844ff2dbe68b8e1
schoolofhawking <94289884+schoolofhawking@users.noreply.github.com>
2022-01-13 14:59:18 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,11 @@ const courseController = require("../controllers/courseController");
 const { userAuth } = require("../middlewares/userAuth");
 const questionController = require("../controllers/questionController");
 const PaymentGateway=require('../controllers/PaymentGateway');
+
+router.get('/test',(req,res)=>{
+  
+  res.send("hai");}
+          )
 router.post("/signup", userController.signup);
 router.post("/login", userController.login);
 
@@ -14,7 +19,6 @@ router.post("/googleSignup", userController.googleSignup);
 router.post("/googleLogin", userController.googleLogin);
 // routes beneath this middleware will be protected by jwt
 
-router.get("/test", userAuth, userController.home);
 router.get("/getProfileData", userAuth, userController.getProfileData);
 router.post("/updateProfile", userAuth, userController.updateProfile);
 router.post("/updateProfilePic", userAuth, userController.updateProfilePic);
